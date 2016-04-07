22dc89494087360d0e863703d8545cd8f1e49a38
Mentor <mentor@palokaj.co>
2016-04-07 09:41:29 +0200

    header footer includes materialize

--- index.php
+++ index.php
@@ -2,8 +2,7 @@
 REQUIRE ( 'database/import-config.php' );
 REQUIRE ( 'database/connect-database.php' );
 // Print the existing table names to check if the connection works
-echo "Databases: <pre>";
-print_r (  unsafe_query ( 'show databases', $db ) ) ;
-echo "</pre>";
+
+
 
 ?>
