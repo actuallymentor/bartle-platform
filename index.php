b0834b2d936e4608c338d2ab7b27002155cc898f
Mentor <mentor@palokaj.co>
2016-04-07 09:47:05 +0200

    html structure

--- index.php
+++ index.php
@@ -1,8 +1,17 @@
 <?php
 REQUIRE ( 'database/import-config.php' );
 REQUIRE ( 'database/connect-database.php' );
-// Print the existing table names to check if the connection works
+REQUIRE ( 'includes/header.php' ); 
+?>
 
 
+<!-- HTML segment start -->
+
+
+<!-- HTML segment start -->
+
+<?php
+
+REQUIRE ( 'includes/footer.php' ); 
 
 ?>
