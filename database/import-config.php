e2f1d38284057d2b6c94b20a7427f95273524ecb
Mentor <mentor@palokaj.co>
2016-04-14 19:56:03 +0300

    used php file instead of config ini

--- database/import-config.php
+++ database/import-config.php
@@ -1,11 +1,20 @@
 <?php
 // This file imports the config ini and can be used to define other central helper resources
-
+if ( ! defined('ABSPATH')) exit('How about no');
 ///////////////////
 // Get the ini file
 ///////////////////
 shell_exec ( 'touch database/config.ini' ); 
-$config = parse_ini_file( 'database/config.ini' );
+$config = [
+	'debug' => true,
+	'db' => [
+		'type' => 'mysql',
+		'host' => 'localhost',
+		'user' => 'root',
+		'password' => 'root',
+		'name' => 'thesis_bartle'
+			]
+];
 
 // Error logging function
 
